54cf70c6c7fd920ead9ebcbda88368dd629e268a
Yana Zharkova <yana04021996@gmail.com>
2016-05-02 17:56:44 +0600

    LinesCounter
    первая версия программы

---
+++ Zharkova.Nsudotnet.LinesCounter/Zharkova.Nsudotnet.LinesCounter/LineCounter.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+
+
+namespace Zharkova.Nsudotnet.LinesCounter
+{
+    public class LineCounter
+    {
+        private readonly string _extension;
+
+        public LineCounter(string extension)
+        {
+            _extension = extension;
+        }
+
+        public void CountLines()
+        {
+            var files = Directory.EnumerateFiles(Directory.GetCurrentDirectory(), "*." + _extension, SearchOption.AllDirectories);
+            var fileNumber = 0;
+            var lineNumber = 0;
+            foreach (var f in files)
+            {
+                var reader = new StreamReader(f);
+                var count = 0;
+                var isComment = false;
+                var isOneLineComment = false;
+                var isNewLine = true;
+                while (!reader.EndOfStream)
+                {
+                    var ch = (char)reader.Read();
+                    if (ch == '\r' || ch == ' ')
+                    {
+                        continue;
+                    }
+
+                    if (ch == '/' && !isOneLineComment)
+                    {
+                        ch = (char)reader.Read();
+                        if (ch == 0)
+                        {
+                            break;
+                        }
+
+                        if (ch == '*')
+                        {
+                            isComment = true;
+                            continue;
+
+                        }
+                        if (ch == '/' && !isComment)
+                        {
+                            isOneLineComment = true;
+                            continue;
+                        }
+                    }
+
+                    if (ch == '*' && isComment)
+                    {
+                        ch = (char)reader.Read();
+                        if (ch == 0)
+                        {
+                            break;
+                        }
+                        if (ch == '/')
+                        {
+                            isComment = false;
+                            continue;
+                        }
+                    }
+                    if (ch == '\n')
+                    {
+                        isNewLine = true;
+                        if (isOneLineComment)
+                        {
+                            isOneLineComment = false;
+                        }
+                        continue;
+                    }
+
+                    if (isNewLine && !isComment && !isOneLineComment)
+                    {
+                        count++;
+                        isNewLine = false;
+                    }
+                }
+                fileNumber++;
+                lineNumber += count;
+            }
+            Console.WriteLine("{1} lines in {0} files", fileNumber, lineNumber);
+        }
+    }
+}
---
+++ Zharkova.Nsudotnet.LinesCounter/Zharkova.Nsudotnet.LinesCounter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Zharkova.Nsudotnet.LinesCounter
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Not enough arguments. Usage: extension");
+                Console.ReadKey();
+                return;
+            }
+            var lineCounter = new LineCounter(args[0]);
+            lineCounter.CountLines();
+            Console.ReadKey();
+        }
+    }
+}
