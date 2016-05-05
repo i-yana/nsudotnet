649ce7297aac20b58b553545c195378eebf09397
Yana Zharkova <yana04021996@gmail.com>
2016-05-05 15:27:44 +0600

    Disposable
    исправлено закрытие файла

--- Zharkova.Nsudotnet.LinesCounter/Zharkova.Nsudotnet.LinesCounter/LineCounter.cs
+++ Zharkova.Nsudotnet.LinesCounter/Zharkova.Nsudotnet.LinesCounter/LineCounter.cs
@@ -20,71 +20,73 @@ namespace Zharkova.Nsudotnet.LinesCounter
             var lineNumber = 0;
             foreach (var f in files)
             {
-                var reader = new StreamReader(f);
-                var count = 0;
-                var isComment = false;
-                var isOneLineComment = false;
-                var isNewLine = true;
-                while (!reader.EndOfStream)
+                using (var reader = new StreamReader(f))
                 {
-                    var ch = (char)reader.Read();
-                    if (ch == '\r' || ch == ' ')
+                    var count = 0;
+                    var isComment = false;
+                    var isOneLineComment = false;
+                    var isNewLine = true;
+                    while (!reader.EndOfStream)
                     {
-                        continue;
-                    }
-
-                    if (ch == '/' && !isOneLineComment)
-                    {
-                        ch = (char)reader.Read();
-                        if (ch == 0)
+                        var ch = (char) reader.Read();
+                        if (ch == '\r' || ch == ' ')
                         {
-                            break;
-                        }
-
-                        if (ch == '*')
-                        {
-                            isComment = true;
-                            continue;
-
-                        }
-                        if (ch == '/' && !isComment)
-                        {
-                            isOneLineComment = true;
                             continue;
                         }
-                    }
 
-                    if (ch == '*' && isComment)
-                    {
-                        ch = (char)reader.Read();
-                        if (ch == 0)
+                        if (ch == '/' && !isOneLineComment)
                         {
-                            break;
+                            ch = (char) reader.Read();
+                            if (ch == 0)
+                            {
+                                break;
+                            }
+
+                            if (ch == '*')
+                            {
+                                isComment = true;
+                                continue;
+
+                            }
+                            if (ch == '/' && !isComment)
+                            {
+                                isOneLineComment = true;
+                                continue;
+                            }
                         }
-                        if (ch == '/')
+
+                        if (ch == '*' && isComment)
                         {
-                            isComment = false;
+                            ch = (char) reader.Read();
+                            if (ch == 0)
+                            {
+                                break;
+                            }
+                            if (ch == '/')
+                            {
+                                isComment = false;
+                                continue;
+                            }
+                        }
+                        if (ch == '\n')
+                        {
+                            isNewLine = true;
+                            if (isOneLineComment)
+                            {
+                                isOneLineComment = false;
+                            }
                             continue;
                         }
-                    }
-                    if (ch == '\n')
-                    {
-                        isNewLine = true;
-                        if (isOneLineComment)
-                        {
-                            isOneLineComment = false;
-                        }
-                        continue;
-                    }
 
-                    if (isNewLine && !isComment && !isOneLineComment)
-                    {
-                        count++;
-                        isNewLine = false;
+                        if (isNewLine && !isComment && !isOneLineComment)
+                        {
+                            count++;
+                            isNewLine = false;
+                        }
                     }
+                    fileNumber++;
+                    lineNumber += count;
                 }
-                fileNumber++;
-                lineNumber += count;
             }
             Console.WriteLine("{1} lines in {0} files", fileNumber, lineNumber);
         }
